c6c866602bfb4f701c488c26c24ce5f019314d42
basicPrototype <rmarquis@gmail.com>
2020-06-10 08:56:43 -0400

    add new music module

---
+++ src/app/features/music/music.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MusicComponent } from './music.component';
+
+
+
+@NgModule({
+  declarations: [MusicComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [MusicComponent]
+})
+export class MusicModule { }
