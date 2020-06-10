3a428a88c5e1961f71087f0b62c336cdce8dde70
basicPrototype <rmarquis@gmail.com>
2020-06-10 10:18:06 -0400

    added reducer

--- src/app/features/music/reducers/index.ts
+++ src/app/features/music/reducers/index.ts
@@ -1,10 +1,11 @@
 import { ActionReducerMap } from '@ngrx/store';
 export const featureName = 'music';
+import * as fromSongs from './songs.reducer';
 
 export interface MusicState {
-
+  songs: fromSongs.SongState;
 }
 
 export const reducers: ActionReducerMap<MusicState> = {
-
+  songs: fromSongs.reducer
 };
--- src/app/features/music/reducers/songs.reducer.ts
+++ src/app/features/music/reducers/songs.reducer.ts
@@ -1,15 +1,19 @@
 import { EntityState, createEntityAdapter } from '@ngrx/entity';
 import { createReducer, Action } from '@ngrx/store';
 
-export interface EntityInterface {
+// yes, we're duplicating the model elements on purpose.  don't reuse your model here.
+export interface SongEntity {
+  id: string;
+  title: string;
+  artist: string;
+  album: string;
+}
+
+export interface SongState extends EntityState<SongEntity> {
 
 }
 
-export interface State extends EntityState<EntityInterface> {
-
-}
-
-export const adapter = createEntityAdapter<EntityInterface>();
+export const adapter = createEntityAdapter<SongEntity>();
 
 const initialState = adapter.getInitialState();
 
@@ -17,7 +21,7 @@ const reducerFunction = createReducer(
   initialState
 );
 
-export function reducer(state: State = initialState, action: Action) {
+export function reducer(state: SongState = initialState, action: Action) {
   return reducerFunction(state, action);
 }
 
