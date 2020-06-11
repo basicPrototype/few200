1980313bc2f9fd31833981042fd464578f550a7e
basicPrototype <rmarquis@gmail.com>
2020-06-11 10:45:52 -0400

    displaying books with sample data

--- src/app/features/books/list/list.component.ts
+++ src/app/features/books/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookListItem } from '../models/book.model';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+
+  books: BookListItem[] = [
+    { id: '1', title: 'It', author: 'Stephen King', format: 'eBook' },
+    { id: '2', title: 'Lonesome Dove', author: 'Larry McMurtry', format: 'Hardcover' }
+  ];
 
   constructor() { }
 
