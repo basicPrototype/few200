26e7005f5408c1891c2fedb4cc6cf8de67447caa
basicPrototype <rmarquis@gmail.com>
2020-06-11 09:58:22 -0400

    added entry, list module.  added to books html

--- src/app/features/books/books.module.ts
+++ src/app/features/books/books.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BooksComponent } from './books.component';
+import { EntryComponent } from './entry/entry.component';
+import { ListComponent } from './list/list.component';
 
 
 
 @NgModule({
-  declarations: [BooksComponent],
+  declarations: [BooksComponent, EntryComponent, ListComponent],
   imports: [
     CommonModule
   ]
